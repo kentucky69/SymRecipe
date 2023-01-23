c9b04c036be88b000c60ed9bfd436ff4b059f92a
ken <ken.gassy@gmail.com>
2023-01-23 19:12:56 +0100

    👍🏻Ajustement Faker load Random Name

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,28 +6,30 @@ use App\Entity\Ingredient;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\DBAL\Driver\IBMDB2\Exception\Factory;
 use Doctrine\Persistence\ObjectManager;
-use Generator;
+use Faker;
 
 class AppFixtures extends Fixture
 {
 
-    /**
-     * @var Genarator
-     */
-    private Generator $faker;
+    // /**
+    //  * @var Genarator
+    //  */
+    // private Generator $faker;
 
-    public function __construct()
-    {
-        $this->faker = Factory::create('fr_FR');
-    }
+    // public function __construct()
+    // {
+    //     $this->faker = Factory::create('fr_FR');
+    // }
 
     public function load(ObjectManager $manager): void
     {
+        $faker = Faker\Factory::create('fr_FR');
 
-        
+        $name = array();
+
         for ($i = 0; $i < 50; $i++) {
             $ingredient = new Ingredient();
-            $ingredient->setName($this->faker->word)
+            $ingredient->setName($faker->lastName())
                 ->setPrice(mt_rand(0, 100));
 
             $manager->persist($ingredient);
