ba1611dc4008ae3fb7e2c1016f98ee7e676f1b63
ken <ken.gassy@gmail.com>
2023-01-24 21:13:53 +0100

    👨🏻‍💻Initialisation Paginotor Knpt

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -2,19 +2,28 @@
 
 namespace App\Controller;
 
-use App\Entity\Ingredient;
 use App\Repository\IngredientRepository;
+use Doctrine\ORM\Tools\Pagination\PaginatorInterface;
+use Knp\Component\Pager\PaginatorInterface as PagerPaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\BrowserKit\Request;
+use Symfony\Component\HttpFoundation\Request as HttpFoundationRequest;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class IngredientController extends AbstractController
 {
     #[Route('/ingredient', name: 'app_ingredient')]
-    public function index(IngredientRepository $repository): Response
+    public function index(IngredientRepository $repository, PagerPaginatorInterface $paginator, HttpFoundationRequest $request): Response
     {
+        $ingredients = $paginator->paginate(
+            $repository->findAll(),
+            $request->query->getInt('page', 1),
+            10
+        );
+
         return $this->render('pages/ingredient/index.html.twig', [
-            'ingredients' => $repository->findAll()
+            'ingredients' => $ingredients
         ]);
     }
 }
